80a579c039c29646a7827200ecc9aa2c79e76bd4
luoxin <70895770+luoxinggyyy@users.noreply.github.com>
2023-08-31 08:50:51 +0800

    Update ModelCondition1.py

--- DiffusionFreeGuidence/ModelCondition1.py
+++ DiffusionFreeGuidence/ModelCondition1.py
@@ -8,12 +8,12 @@ from torch.nn import init
 from torch.nn import functional as F
 from  tensorboardX import SummaryWriter
 from torchsummary import summary
-def drop_connect(x, drop_ratio): #随机丢弃，drop-ratio为丢弃率
+def drop_connect(x, drop_ratio): 
     keep_ratio = 1.0 - drop_ratio
     mask = torch.empty([x.shape[0], 1, 1, 1], dtype=x.dtype, device=x.device)
     mask.bernoulli_(p=keep_ratio)
     x.div_(keep_ratio)
-    x.mul_(mask) #张量与mask相乘
+    x.mul_(mask) 
     return x
 
 class Swish(nn.Module):
@@ -21,7 +21,7 @@ class Swish(nn.Module):
         return x * torch.sigmoid(x)
 
 
-class TimeEmbedding(nn.Module): #时间位置编码
+class TimeEmbedding(nn.Module): 
     def __init__(self, T, d_model, dim):
         assert d_model % 2 == 0
         super().__init__()
@@ -46,7 +46,7 @@ class TimeEmbedding(nn.Module): #时间位置编码
         return emb
 
 
-# class ConditionalEmbedding(nn.Module): #条件编码
+# class ConditionalEmbedding(nn.Module): 
 #     def __init__(self, num_labels, d_model, dim):
 #         assert d_model % 2 == 0
 #         super().__init__()
@@ -60,7 +60,7 @@ class TimeEmbedding(nn.Module): #时间位置编码
 #     def forward(self, t):
 #         emb = self.condEmbedding(t)
 #         return emb
-class ConditionalEmbedding2(nn.Module): #条件编码
+class ConditionalEmbedding2(nn.Module): 
     def __init__(self, d_model, dim):
         assert d_model % 2 == 0
         super().__init__()
@@ -73,7 +73,7 @@ class ConditionalEmbedding2(nn.Module): #条件编码
         emb = self.condEmbedding2(t)
         return emb
     
-class ConditionalEmbedding1(nn.Module): #条件编码，这个是针对孔隙度调整的
+class ConditionalEmbedding1(nn.Module): 
     def __init__(self, d_model, dim):
         assert d_model % 2 == 0
         super().__init__()
@@ -88,14 +88,14 @@ class ConditionalEmbedding1(nn.Module): #条件编码，这个是针对孔隙度
         return emb
 
     
-class DownSample(nn.Module):  #下采样
+class DownSample(nn.Module): 
     def __init__(self, in_ch):
         super().__init__()
         self.main = nn.Conv3d(in_ch, in_ch, 3, stride=2, padding=1)
         self.c2 = nn.Conv3d(in_ch, in_ch, 5, stride=2, padding=2)
 
     def forward(self, x, temb, cemb1,cemb2):
-        # x = self.c1(x) + self.c2(x)  #拼接
+        # x = self.c1(x) + self.c2(x)  
         x = self.c2(x)
         return x
 
@@ -113,10 +113,10 @@ class UpSample(nn.Module):
         return x
 
 
-class AttnBlock(nn.Module):  #注意力机制模块
+class AttnBlock(nn.Module): 
     def __init__(self, in_ch):
         super().__init__()
-        self.group_norm = nn.GroupNorm(8, in_ch) #归一化，是bn层的变种
+        self.group_norm = nn.GroupNorm(8, in_ch) 
         self.proj_q = nn.Conv3d(in_ch, in_ch, 1, stride=1, padding=0)
         self.proj_k = nn.Conv3d(in_ch, in_ch, 1, stride=1, padding=0)
         self.proj_v = nn.Conv3d(in_ch, in_ch, 1, stride=1, padding=0)
@@ -124,8 +124,7 @@ class AttnBlock(nn.Module):  #注意力机制模块
 
 
 
-    def forward(self, x): #torch-bmm函数 ：一个批量矩阵乘法操作，计算查询和关键特征图之间的点积，
-                                           #代表了输入特征图中每一对空间位置之间的注意权重。
+    def forward(self, x): 
         B, C, M, H, W = x.shape
         h = self.group_norm(x)
         q = self.proj_q(h)
@@ -139,16 +138,16 @@ class AttnBlock(nn.Module):  #注意力机制模块
         w = F.softmax(w, dim=-1)
 
         v = v.permute(0, 2, 3, 4, 1).view(B, M * H * W, C)
-        h = torch.bmm(w, v)  #这里使用注意力权重w和特征map-v的加权和。这产生了一个新的特征图h
+        h = torch.bmm(w, v) 
         assert list(h.shape) == [B, M * H * W, C]
         h = h.view(B, M, H, W, C).permute(0, 4, 1, 2, 3)
         h = self.proj(h)
 
-        return x + h   #这里是输出原图x与注意力特征map-h的和
+        return x + h   
 
 
 
-class ResBlock(nn.Module):  #残差模块
+class ResBlock(nn.Module):  
     def __init__(self, in_ch, out_ch, tdim, dropout, attn=False):
         super().__init__()
         self.block1 = nn.Sequential(
@@ -182,18 +181,18 @@ class ResBlock(nn.Module):  #残差模块
 
     def forward(self, x, temb, labels,label2):
         h = self.block1(x)
-        h += self.temb_proj(temb)[:, :, None, None, None] #时间
+        h += self.temb_proj(temb)[:, :, None, None, None] 
         # labels = labels.to('cuda:0')
-        h += self.cond_proj(labels)[:, :, None, None, None] #标签
-        h += self.cond_proj(label2)[:, :, None, None, None] #标签
+        h += self.cond_proj(labels)[:, :, None, None, None] 
+        h += self.cond_proj(label2)[:, :, None, None, None] 
         h = self.block2(h)
 
         h = h + self.shortcut(x)
         h = self.attn(h)
-        return h  #该函数的输出是剩余连接和注意力块的输出（如果attn为真）或卷积块的输出（如果attn为假）之和
+        return h 
 
 
-class UNet(nn.Module): #unet 来进行编码解码
+class UNet(nn.Module): 
     def __init__(self, T, ch, ch_mult, num_res_blocks, dropout):
         super().__init__()
         tdim = ch * 4
@@ -248,9 +247,9 @@ class UNet(nn.Module): #unet 来进行编码解码
         temb = self.time_embedding(t)
         # cemb = self.cond_embedding1(labels)
         cemb1 = self.cond_embedding1(labels).to('cuda') 
-        cemb2 = self.cond_embedding2(label2).to('cuda')  #以作测试，放在cpu上的
+        cemb2 = self.cond_embedding2(label2).to('cuda') 
         # cemb = rockEmbedding(labels,512).to('cuda')
-        # cemb = labels.repeat(32,1).transpose(0,1).requires_grad_(True)  #这两个是将通道扩张成512，结果发现gpu计算不够
+        # cemb = labels.repeat(32,1).transpose(0,1).requires_grad_(True)  
         # Downsampling
         # x = self.bn(x)
 
